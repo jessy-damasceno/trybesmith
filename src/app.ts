f3b331222c5436b28f0f8b652129ccd57d73cc04
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-06 15:28:30 -0300

    feat: add errorMiddleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/errorMiddleware';
+import productsRouter from './models/routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRouter);
+
+app.use(errorMiddleware);
 
 export default app;

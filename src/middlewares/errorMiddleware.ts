84b8478c413de7cf3760f56db6cd033828a89739
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-06 15:38:05 -0300

    refatore: errorMiddleware err parameter type

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import IError from '../interfaces/@error';
 import { mapError } from '../utils/errorMap';
 
-const errorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  const { type, message } = err as IError;
+const errorMiddleware = (err: IError, _req: Request, res: Response, _next: NextFunction) => {
+  const { type, message } = err;
   return res.status(mapError(type)).json({ message });
 };
 

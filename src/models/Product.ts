26f8f7f1e0a0a8342d08553bd0391831e09191ec
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-06 16:30:17 -0300

    feat: create Product model

---
+++ src/models/Product.ts
@@ -0,0 +1,28 @@
+import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import { IProduct } from '../interfaces';
+import connection from './connection';
+
+export default class Product {
+  private connection: Pool;
+
+  constructor() {
+    this.connection = connection;
+  }
+
+  public getAll = async (): Promise<IProduct> => {
+    const [result] = await this.connection
+      .execute<IProduct & RowDataPacket[]>('SELECT * FROM Trybesmith.Products');
+
+    return result as IProduct;
+  };
+
+  public create = async (product: IProduct): Promise<IProduct> => {
+    const { name, amount } = product;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    return { id: insertId, ...product };
+  };
+}
